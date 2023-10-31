c3b344e280170c38a44abb6df100b350559e428d
cpapanicholas <cpapanicholas@gmail.com>
2023-10-30 22:24:06 -0500

    added code to product-routes.js ot finished with it yet

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -44,26 +44,25 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
+// update a category by its `id` value
 router.put('/:id', async (req, res) => {
-  // update a category by its `id` value
-  router.put('/:id', async (req, res) => {
-    try {
-      const category = await Category.findByPk(req.params.id);
-      if (!category) {
-        res.status(404).json({ message: 'Category not found' });
-        return;
-      }
-  
-      // Update the category's properties
-      await category.update(req.body);
-  
-      res.status(200).json(category);
-    } catch (err) {
-      res.status(500).json(err);
-    }});
+  try {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+      res.status(404).json({ message: 'Category not found' });
+      return;
+    }
 
+    // Update the category's properties
+    await category.update(req.body);
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
